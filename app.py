90a276130ef4e6bad5d069bc9a784f69fdacd863
Bastien Wermeille <bastien.wermeille@gmail.com>
2019-03-09 10:58:18 +0100

    create programme structure

--- app.py
+++ app.py
@@ -9,6 +9,45 @@
 # - Bastien Wermeille
 
 
+class Client:
+    def __init__(self, name):
+        self.name = name
+
+    def connect(self, server):
+        print(self.name + " connect to " + server.name)
+        nonce = server.get_nonce()
+
+        # Hash password
+        hashed_pass = "TODO"
+        server.auth(hashed_pass)
+
+        server.send_message()
+
+class Server:
+    def __init__(self, name, password_database):
+        self.name = name
+        self.password_database = password_database
+
+    def get_nonce(self):
+        pass
+        # TODO Generate random nonce
+
+    def auth(self, hashed_pass):
+        pass
+    
+    def send_message(self):
+        pass
+
 if __name__ == '__main__':
     #TODO
-    pass
+
+    password_database{
+        'Alice': 'a9d9gV!@P8L)<=a?[kMGV]\>AGe-~dFbuTE]u<qv/`F5Tcf7m/',
+        'Jon': '9-a/3s\~UQ@R-tq3=qJDAy)MFbAg68Psf\f59g,F*hQ`*R6#+c',
+        'Emilia': '9-a/3s\~UQ@R-tq3=qJDAy)MFbAg68Psf\f59g,F*hQ`*R6#+c'
+    }
+
+    alice = Client('Alice', password_database['Alice'])
+    bob = Server('Bob', password_database)
+
+    alice.connect(bob)
