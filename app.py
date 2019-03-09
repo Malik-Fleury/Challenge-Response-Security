c80916dd525ed4e887d72ee66b23d41f0d2f01f7
Bastien Wermeille <bastien.wermeille@gmail.com>
2019-03-09 11:08:37 +0100

    Improve structure and implement CHAP logic

--- app.py
+++ app.py
@@ -10,16 +10,17 @@
 
 
 class Client:
-    def __init__(self, name):
+    def __init__(self, name, password):
         self.name = name
+        self.password = password
 
     def connect(self, server):
         print(self.name + " connect to " + server.name)
-        nonce = server.get_nonce()
+        nonce = server.get_nonce(self.name)
 
         # Hash password
-        hashed_pass = "TODO"
-        server.auth(hashed_pass)
+        hashed_pass = "TODO hash current password"
+        server.auth(self.name, hashed_pass)
 
         server.send_message()
 
@@ -27,13 +28,22 @@ class Server:
     def __init__(self, name, password_database):
         self.name = name
         self.password_database = password_database
+        self.nonce_database = {}
 
-    def get_nonce(self):
-        pass
-        # TODO Generate random nonce
+    def get_nonce(self, username):
+        nonce = "TODO Random nonce"
+        self.nonce_database[username] = {
+            'validity':12,
+            'value': nonce
+        }
+        return nonce
 
-    def auth(self, hashed_pass):
-        pass
+
+    def auth(self, username, hashed_pass):
+        try:
+            return password_database[username] == hashed_pass
+        except Exception:
+            return False
     
     def send_message(self):
         pass
@@ -41,7 +51,7 @@ class Server:
 if __name__ == '__main__':
     #TODO
 
-    password_database{
+    password_database = {
         'Alice': 'a9d9gV!@P8L)<=a?[kMGV]\>AGe-~dFbuTE]u<qv/`F5Tcf7m/',
         'Jon': '9-a/3s\~UQ@R-tq3=qJDAy)MFbAg68Psf\f59g,F*hQ`*R6#+c',
         'Emilia': '9-a/3s\~UQ@R-tq3=qJDAy)MFbAg68Psf\f59g,F*hQ`*R6#+c'
